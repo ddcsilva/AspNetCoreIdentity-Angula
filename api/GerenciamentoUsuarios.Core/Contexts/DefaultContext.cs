6f1b1444eee44a7833843890a3360cd1fe3079bf
Danilo <danilo.silva@msn.com>
2023-04-19 22:35:53 -0300

    Criação dos Projetos de API e Core Implementação do Context do Identity

---
+++ api/GerenciamentoUsuarios.Core/Contexts/DefaultContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoUsuarios.Core.Contexts
+{
+    public class DefaultContext : IdentityDbContext<Usuario, Funcao, int>
+    {
+        public DefaultContext(DbContextOptions<DefaultContext> options) : base (options)
+        {
+        }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Morador> Moradores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
